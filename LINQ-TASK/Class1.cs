4fbe70fabdc09ce864f7aecf50fe81e90877ef0f
Nazar <nazar.hrytsiv7@gmail.com>
2018-05-28 00:26:37 +0300

    fixed bugs

---
+++ LINQ-TASK/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace LINQ_TASK
+{
+    class Client
+    {
+        public int Code { get; set; }
+        public int Year { get; set; }
+        public int Mounth { get; set; }
+        public int Duration_of_classes { get; set; }
+        public void read_from_file_to_client(StreamReader reader)
+        {
+            var list = reader.ReadLine().Split(' ').ToList().Select(Int32.Parse).ToList();
+            this.Code = list[0];
+            this.Year = list[1];
+            this.Mounth = list[2];
+            this.Duration_of_classes = list[3];
+        }
+        
+        public static Client find_longest_duration(List<Client> clients)
+        {
+            int duration = clients.Max(x => x.Duration_of_classes);
+            Client client_longest_duration = clients.First(x => x.Duration_of_classes == duration);
+            return client_longest_duration;
+        }
+
+        public static Client find_shorted_duration(List<Client> clients)
+        {
+            int duration = clients.Min(item => item.Duration_of_classes);
+            Client client_shorted_duration = clients.First(item => item.Duration_of_classes == duration);
+            return client_shorted_duration;
+        }
+
+        public static int find_year(List<Client> clients)
+        {
+            IEnumerable<IGrouping<int, Client>> group_year = clients.GroupBy(item => item.Year);
+            IEnumerable<IGrouping<int, Client>> sorted = group_year.OrderByDescending(group => group.Sum(client => client.Duration_of_classes)).ThenBy(x => x.Min(y => y.Year));
+            return sorted.First().Key;
+        }
+
+        public static SortedDictionary<int, int> sum_durations(List<Client> clients)
+        {
+            IEnumerable<IGrouping<int, Client>> group_by = clients.GroupBy(item => item.Code);
+            SortedDictionary<int, int> durations = new SortedDictionary<int, int>();
+            foreach (IGrouping<int, Client> group in group_by)
+            {
+                int sum = group.Sum(x => x.Duration_of_classes);
+                durations.Add(sum, group.Key);
+            }
+
+            return durations;
+        }
+
+        public static SortedDictionary<int, int> count_months(List<Client> clients)
+        {
+            IEnumerable<IGrouping<int, Client>> group_by_months = clients.GroupBy(item => item.Code);
+            SortedDictionary<int, int> months = new SortedDictionary<int, int>();
+            foreach (IGrouping<int, Client> groups in group_by_months)
+            {
+                int count = groups.Count();
+                months.Add(count, groups.Key);
+            }
+
+            return months;
+        }
+
+        public static List<Client> read_from_file(StreamReader reader, int len)
+        {
+            List<Client> clients = new List<Client>();
+            for (int i = 0; i < len; i++)
+            {
+                Client client = new Client();
+                client.read_from_file_to_client(reader);
+                clients.Add(client);
+            }
+            return clients;
+        }
+    }
+}
--- LINQ-TASK/Program.cs
+++ LINQ-TASK/Program.cs
@@ -11,37 +11,29 @@ namespace LINQ_TASK
         static void Task16()
         {
             List<string> numbers = DTO.read_from_file("data.txt");
-
-            IEnumerable<string> answer = numbers.Where(number => Int32.Parse(number) > 0);
-
-            DTO.write_to_file("answer.txt", answer);
+            var answer = numbers.Where(number => Int32.Parse(number) > 0);
+            DTO.write_to_file("out1.txt", answer);
         }
 
         static void Task17()
         {
             List<string> numbers = DTO.read_from_file("data17.txt");
-
-            IEnumerable<string> answer = numbers.Where(number => (Int32.Parse(number) % 2 == 1 || Int32.Parse(number) % 2 == -1)).Distinct();
-
-            DTO.write_to_file("answer17.txt", answer);
+            var answer = numbers.Where(number => (Int32.Parse(number) % 2 == 1 || Int32.Parse(number) % 2 == -1)).Distinct();
+            DTO.write_to_file("out17.txt", answer);
         }
 
         static void Task18()
         {
             List<string> numbers = DTO.read_from_file("data18.txt");
-
-            IEnumerable<string> answer = numbers.OrderBy(number => Int32.Parse(number)).Where(number => ((number.StartsWith("-") == false) && (number.Length == 2)));
-
-            DTO.write_to_file("answer18.txt", answer);
+            var answer = numbers.OrderBy(number => Int32.Parse(number)).Where(number => ((number.StartsWith("-") == false) && (number.Length == 2)));
+            DTO.write_to_file("out18.txt", answer);
         }
 
         static void Task19()
         {
             List<string> words = DTO.read_from_file("data19.txt");
-
-            IEnumerable<string> answer = words.OrderBy(word => word.Length).ThenByDescending(word => word);
-
-            DTO.write_to_file("answer19.txt", answer);
+            var answer = words.OrderBy(word => word.Length).ThenByDescending(word => word);
+            DTO.write_to_file("out19.txt", answer);
         }
 
       
@@ -52,26 +44,78 @@ namespace LINQ_TASK
             int D = Int32.Parse(reader.ReadLine());
             List<string> numbers = reader.ReadToEnd().Split(' ').ToList();
             reader.Close();
-
-            IEnumerable<string> answer_numbers = numbers.SkipWhile(number => Int32.Parse(number) <= D).Reverse().Where(number => Int32.Parse(number) % 2 == 1);
-
-            FileStream answer = new FileStream("answer20.txt", FileMode.Create);
-            StreamWriter writer = new StreamWriter(answer);
-            foreach (var number in answer_numbers)
-            {
-                writer.Write($"{number} ");
-            }
-            writer.Close();
+            var answer_numbers = numbers.SkipWhile(number => Int32.Parse(number) <= D).Reverse().Where(number => Int32.Parse(number) % 2 == 1);
+            DTO.write_to_file("out20.txt", answer_numbers);
         }
 
-        
-        static void Main(string[] args)
+        static void Task44()
+        {
+            FileStream datum = new FileStream("data44.txt", FileMode.Open);
+            StreamReader reader = new StreamReader(datum);
+            int K1 = Convert.ToInt32(reader.ReadLine());
+            int K2 = Convert.ToInt32(reader.ReadLine());
+            List<string> a = reader.ReadLine().Split(' ').ToList();
+            List<string> b = reader.ReadLine().Split(' ').ToList();
+            reader.Close();
+            var listA = a.Where(n => Int32.Parse(n) > K1);
+            var listB = b.Where(n => Int32.Parse(n) < K2);
+            var answer = listA.Concat(listB).OrderBy(n => Int32.Parse(n));
+            DTO.write_to_file("out44.txt", answer);
+            
+        }
+
+        static void Task45()
+        {
+            FileStream datum = new FileStream("data45.txt", FileMode.Open);
+            StreamReader reader = new StreamReader(datum);
+            int L1 = Convert.ToInt32(reader.ReadLine());
+            int L2 = Convert.ToInt32(reader.ReadLine());
+            List<string> a = reader.ReadLine().Split(' ').ToList();
+            List<string> b = reader.ReadLine().Split(' ').ToList();
+            reader.Close();
+            var listA = a.Where(x => x.Length == L1);
+            var listB = b.Where(x => x.Length == L2);
+            var answer = listA.Concat(listB).OrderByDescending(n => n);
+            DTO.write_to_file("out45.txt", answer);
+        }
+
+        static void TaskLast()
+        {
+            FileStream datum = new FileStream("dataLast.txt", FileMode.Open);
+            StreamReader reader = new StreamReader(datum);
+            int len = Int32.Parse(reader.ReadLine());
+            List<Client> clients = Client.read_from_file(reader, len);
+            reader.Close();
+            FileStream datum_out = new FileStream("outLast.txt", FileMode.Create);
+            StreamWriter writer = new StreamWriter(datum_out);
+            writer.WriteLine($"Min duration: {Client.find_shorted_duration(clients).Code}");
+            writer.WriteLine($"Year with the biggest sum: {Client.find_year(clients)}");
+            writer.WriteLine($"Max duration: {Client.find_longest_duration(clients).Code}");
+            SortedDictionary<int, int> durations = Client.sum_durations(clients);
+            writer.WriteLine("Sum of durations:");
+            foreach (var answer in durations)
+            {
+                writer.WriteLine($"Value - {answer.Value}, duration - {answer.Key}");
+            }
+            SortedDictionary<int, int> months = Client.count_months(clients);
+            writer.WriteLine("Count of mounths:");
+            foreach (var item in months)
+            {
+                writer.WriteLine($"code - {item.Value}, count - {item.Key}");
+            }
+            writer.Close();
+
+        }
+            static void Main(string[] args)
         {
             Task16();
             Task17();
             Task18();
             Task19();
             Task20();
+            Task44();
+            Task45();
+            TaskLast();
         }
     }
 }
